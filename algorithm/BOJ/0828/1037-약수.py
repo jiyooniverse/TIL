69fc3f6e7851d8c8dc72f093a2588b45c3028c31
bback5812 <bbackyun5812@gmail.com>
2021-10-16 22:05:40 +0900

    fix: directory 구조 변경

--- algorithm/BOJ/0828/1037-약수.py
+++
@@ -1,8 +0,0 @@
-# BOJ 1037 약수
-N = int(input())
-arr = list(map(int, input().split()))
-# 오름차순 정렬 후
-arr.sort()
-
-result = arr[0] * arr[-1]
-print(result)
--- algorithm/BOJ/0829/21756-지우개.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-
-arr = list(range(N + 1))
-
-while len(arr) > 2:
-    new_arr = arr[::2]
-    arr = new_arr[:]
-
-print(arr[1])
